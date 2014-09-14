f5e170c9ddd8038053c0c3ec9408957639728d0a
Thiago Mata <thiago.henrique.mata@gmail.com>
2014-09-14 19:53:53 -0300

    histogram of color use based on center
    this intent to make easy to create a pattern to detect rotated elements.
    The project was changed to look like the site too.

---
+++ javascripts/demo.js
@@ -0,0 +1,246 @@
+var debugMatrix, drawFixed, drawMatrix, intHeight, intThumbHeight, intThumbWidth, intWidth, makePallete, makeThumbnail, pickColor;
+
+intWidth = 16;
+
+intHeight = 16;
+
+intThumbWidth = 11;
+
+intThumbHeight = 11;
+
+debugMatrix = function(arrMatrix, arrFixedMatrix, strColor) {
+  drawMatrix({
+    red: (strColor === "red" ? arrMatrix : createMatrix(arrMatrix.length, arrMatrix[0].length, 0)),
+    green: (strColor === "green" ? arrMatrix : createMatrix(arrMatrix.length, arrMatrix[0].length, 0)),
+    blue: (strColor === "blue" ? arrMatrix : createMatrix(arrMatrix.length, arrMatrix[0].length, 0))
+  }, document.getElementById("debug-" + strColor));
+  drawFixed(document.getElementById("debug-" + strColor), arrFixedMatrix);
+};
+
+drawFixed = function(objDivMatrix, arrFixedMatrix) {
+  var arrLines, arrPixels, objLine, objPixel, x, y;
+  arrLines = objDivMatrix.childNodes;
+  y = 0;
+  while (y < arrLines.length) {
+    objLine = arrLines[y];
+    arrPixels = objLine.getElementsByTagName("span");
+    x = 0;
+    while (x < arrPixels.length) {
+      objPixel = arrPixels[x];
+      if (arrFixedMatrix[x][y] === 1) {
+        objPixel.style.borderWidth = "1px";
+        objPixel.style.borderColor = "white";
+        objPixel.style.borderStyle = "solid";
+        objPixel.title += " fixed";
+      } else {
+        objPixel.title += " estimated";
+      }
+      x++;
+    }
+    y++;
+  }
+};
+
+pickColor = function(arrMatrix, x, y, notFound) {
+  if (arrMatrix[x] == null) {
+    return notFound;
+  }
+  if (arrMatrix[x][y] == null) {
+    return notFound;
+  }
+  return arrMatrix[x][y];
+};
+
+drawMatrix = function(objMatrixRGB, objDiv) {
+  var arrMatrixBlue, arrMatrixGreen, arrMatrixRed, intBlue, intGreen, intRed, objLine, objPixel, x, y;
+  objDiv.innerHTML = "";
+  arrMatrixRed = objMatrixRGB.red;
+  arrMatrixGreen = objMatrixRGB.green;
+  arrMatrixBlue = objMatrixRGB.blue;
+  y = 0;
+  while (y < arrMatrixRed[0].length) {
+    objLine = document.createElement("div");
+    objLine.className = "line";
+    x = 0;
+    while (x < arrMatrixRed.length) {
+      intRed = Math.round(pickColor(arrMatrixRed, x, y, 255));
+      intGreen = Math.round(pickColor(arrMatrixGreen, x, y, 0));
+      intBlue = Math.round(pickColor(arrMatrixBlue, x, y, 0));
+      objPixel = document.createElement("span");
+      objPixel.className = "pixel";
+      objLine.appendChild(objPixel);
+      objPixel.style.backgroundColor = "rgb(" + intRed + "," + intGreen + "," + intBlue + ")";
+      objPixel.title = "[" + x + "," + y + "] rgb(" + intRed + "," + intGreen + "," + intBlue + ")";
+      objLine.appendChild(objPixel);
+      x++;
+    }
+    objDiv.appendChild(objLine);
+    y++;
+  }
+};
+
+makePallete = function() {
+  var arrColors, intColor, intColors, intElement, objPencil;
+  intColors = 10;
+  arrColors = Array();
+  intColor = 0;
+  while (intColor < 255) {
+    arrColors.push(Array(intColor, intColor, intColor));
+    intColor += intColors;
+  }
+  intColor = 0;
+  while (intColor < 255) {
+    arrColors.push(Array(255 - intColor, 255 - intColor, intColor));
+    intColor += intColors;
+  }
+  intColor = 0;
+  while (intColor < 255) {
+    arrColors.push(Array(intColor, 255 - intColor, 255 - intColor));
+    intColor += intColors;
+  }
+  intColor = 0;
+  while (intColor < 255) {
+    arrColors.push(Array(255 - intColor, intColor, 255 - intColor));
+    intColor += intColors;
+  }
+  intElement = 0;
+  while (intElement < arrColors.length) {
+    objPencil = document.createElement("span");
+    objPencil.className = "pixel";
+    objPencil.style.backgroundColor = "rgb(" + arrColors[intElement].join() + ")";
+    objPencil.onclick = function() {
+      window.pencilColor = this.style.backgroundColor;
+    };
+    document.getElementById("paleta").appendChild(objPencil);
+    intElement++;
+  }
+  window.pencilColor = "rgb(255,0,0)";
+};
+
+window.draw = function() {
+  var color, distance, dx, dy, objLine, objPixel, radio, rgbColor, x, y;
+  x = 0;
+  while (x < intWidth) {
+    objLine = document.createElement("div");
+    objLine.className = "line";
+    y = 0;
+    while (y < intHeight) {
+      objPixel = document.createElement("span");
+      dy = y - intHeight / 2;
+      dx = x - intWidth / 2;
+      distance = Math.sqrt(dx * dx + dy * dy);
+      radio = 7;
+      if (distance < Math.round(radio)) {
+        color = Math.round(255 * (1 - distance / (radio + 1)));
+        rgbColor = "rgb(" + color + "," + color + "," + color + ")";
+        console.log(rgbColor);
+        objPixel.style.backgroundColor = rgbColor;
+      } else {
+        if (x > intWidth * 0.6) {
+          objPixel.style.backgroundColor = "rgb(0,0," + Math.round(200 * x / intWidth) + ")";
+        }
+      }
+      objPixel.className = "pixel";
+      objLine.appendChild(objPixel);
+      objPixel.onclick = function() {
+        this.style.backgroundColor = window.pencilColor;
+        window.makeThumbnail();
+      };
+      y++;
+    }
+    document.getElementById("origins").appendChild(objLine);
+    x++;
+  }
+  window.makeThumbnail();
+};
+
+window.drawHeavy = function ( arrHeavy, objParentDiv, arrColors ) {
+  if( arrColors === undefined ) {
+    arrColors = [200,200,200];
+  }
+  objParentDiv.innerHTML = "";
+  var maxValue = 0;
+  for( var i = 0; i < arrHeavy.length; i++ ) {
+    if( maxValue < arrHeavy[ i ] ) {
+      maxValue = arrHeavy[ i ];
+    }
+  }
+  for( var i = 0; i < arrHeavy.length; i++ ) {
+    var dblProportional = arrHeavy[ i ] / maxValue;
+    var objDiv = document.createElement("div");
+    objDiv.style.backgroundColor = "rgb(" + Math.round( arrColors[0] * dblProportional ) 
+                                    + "," + Math.round( arrColors[1] * dblProportional )
+                                    + "," + Math.round( arrColors[2] * dblProportional ) + ")";
+    objDiv.style.display = "inline-block";
+    objDiv.style.width = "4px";
+    objDiv.style.height = arrHeavy[ i ] / 40 + "px";
+    objParentDiv.appendChild( objDiv );
+  }
+};
+
+window.makeThumbnail = function() {
+  var arrLines, arrMatrixBlue, arrMatrixGreen, arrMatrixRed, arrPixels, objColor, objLine, objPixel, objThumbMatrix, strColor, x, y;
+  arrMatrixRed = createMatrix(intWidth, intHeight, 0);
+  arrMatrixGreen = createMatrix(intWidth, intHeight, 0);
+  arrMatrixBlue = createMatrix(intWidth, intHeight, 0);
+  arrLines = document.getElementById("origins").childNodes;
+  y = 0;
+  while (y < arrLines.length) {
+    objLine = arrLines[y];
+    arrPixels = objLine.getElementsByTagName("span");
+    x = 0;
+    while (x < arrPixels.length) {
+      objPixel = arrPixels[x];
+      strColor = objPixel.style.backgroundColor;
+      if (strColor === false || strColor === undefined || strColor == "" ) {
+        strColor = "rgb(255,255,255)";
+      }
+      objColor = new RGBColor(strColor);
+      arrMatrixRed[x][y] = objColor.r;
+      arrMatrixGreen[x][y] = objColor.g;
+      arrMatrixBlue[x][y] = objColor.b;
+      x++;
+    }
+    y++;
+  }
+  objThumbMatrix = expandMatrixRGB({
+    red: arrMatrixRed,
+    green: arrMatrixGreen,
+    blue: arrMatrixBlue
+  }, intThumbWidth, intThumbHeight);
+  window.thumbMatrix = objThumbMatrix;
+  drawHeavy( getHeavy( window.thumbMatrix.red ), document.getElementById("heavyRed"), [255,200,200] );
+  drawHeavy( getHeavy( window.thumbMatrix.green ), document.getElementById("heavyGreen"), [200,255,200] );
+  drawHeavy( getHeavy( window.thumbMatrix.blue ), document.getElementById("heavyBlue"), [200,200,255] );
+  drawMatrix( objThumbMatrix, document.getElementById("destiny") );
+};
+
+window.addEventListener( "load", function(){
+
+	document.getElementById("thumbWidth").value = intThumbWidth;
+	document.getElementById("showWidth").innerHTML = intThumbWidth;
+	document.getElementById("thumbWidth").addEventListener("change", function() {
+	  intThumbWidth = this.value;
+	  document.getElementById("showWidth").innerHTML = intThumbWidth;
+	  makeThumbnail();
+	});
+
+	document.getElementById("thumbHeight").value = intThumbHeight;
+	document.getElementById("showHeight").innerHTML = intThumbHeight;
+	document.getElementById("thumbHeight").addEventListener("change", function() {
+	  intThumbHeight = this.value;
+	  document.getElementById("showHeight").innerHTML = intThumbHeight;
+	  makeThumbnail();
+	});
+
+	document.getElementById("clearOrigins").addEventListener("click", function() {
+		var arrSpan = document.getElementById("origins").getElementsByTagName("span");
+		for( var i = 0; i < arrSpan.length; i++ ) {
+			arrSpan[i].style.backgroundColor = "rgb(0,0,0)";
+		}
+		makeThumbnail();
+	});
+	makePallete();
+	window.draw();
+
+});
---
+++ javascripts/openpen.js
@@ -0,0 +1,495 @@
+var addColumn, addLine, compareMatrix, createMatrix, createVector, expandMatrix, expandMatrixRGB, loopSmooth, mergeColumns, mergeLines, smoothMatrix, mergeColumnsFixed, mergeLinesFixed;
+
+expandMatrixRGB = function(objMatrixRGB, intNewWidth, intNewHeight) {
+  var arrNewMatrixBlue, arrNewMatrixGreen, arrNewMatrixRed;
+  arrNewMatrixRed = expandMatrix(objMatrixRGB.red, intNewWidth, intNewHeight, "red");
+  arrNewMatrixGreen = expandMatrix(objMatrixRGB.green, intNewWidth, intNewHeight, "green");
+  arrNewMatrixBlue = expandMatrix(objMatrixRGB.blue, intNewWidth, intNewHeight, "blue");
+  return {
+    red: arrNewMatrixRed,
+    green: arrNewMatrixGreen,
+    blue: arrNewMatrixBlue
+  };
+};
+
+expandMatrix = function(arrMatrix, intNewWidth, intNewHeight, strColor) {
+  var arrFixedMatrix, arrMerge, arrNewMatrix, intMatrixHeight, intMatrixWidth, intNewColumn, intNewLine, intProportionX, intProportionY, intXCounter, intYCounter, resultMatrix, x, xGain, xLoop, xWeight, y, yGain, yLoop, yWeight;
+  intMatrixWidth = arrMatrix.length;
+  intMatrixHeight = arrMatrix[0].length;
+  intProportionX = intNewWidth / intMatrixWidth;
+  intProportionY = intNewHeight / intMatrixHeight;
+  arrNewMatrix = arrMatrix;
+  arrFixedMatrix = createMatrix(intMatrixWidth, intMatrixHeight, 1);
+  intXCounter = void 0;
+  x = void 0;
+  if (intNewWidth > intMatrixWidth) {
+    intXCounter = -1 * intProportionX / 2;
+    x = 0;
+    while (x < intNewWidth) {
+      intXCounter += intProportionX;
+      while (intXCounter > 1 && x < intNewWidth) {
+        arrNewMatrix = addColumn(arrNewMatrix, x, 0);
+        arrFixedMatrix = addColumn(arrFixedMatrix, x, 0);
+        intXCounter--;
+        x++;
+      }
+      x++;
+      intXCounter--;
+    }
+    while (arrNewMatrix.length < intNewWidth) {
+      intNewColumn = arrNewMatrix.length - 1;
+      arrNewMatrix = addColumn(arrNewMatrix, intNewColumn, 0);
+      arrFixedMatrix = addColumn(arrFixedMatrix, intNewColumn, 0);
+    }
+  } else {
+    intXCounter = 0;
+    arrMerge = [];
+    x = 0;
+    xLoop = 0;
+    xGain = 0;
+    xWeight = 1;
+    intProportionX = intMatrixWidth / intNewWidth;
+    while (x < intNewWidth) {
+      intXCounter += intProportionX;
+      while (intXCounter > 0) {
+        arrMerge.push({
+          xBefore: xLoop,
+          xAfter: x,
+          weight: xWeight
+        });
+        intXCounter -= xWeight;
+        xGain += xWeight;
+        xWeight = 1 - xWeight;
+        if (xWeight === 0) {
+          xWeight = Math.min(1, intXCounter);
+        }
+        if (xGain === 1) {
+          xLoop++;
+          xWeight = Math.min(1, intXCounter);
+          xGain = 0;
+        }
+      }
+      x++;
+    }
+    arrNewMatrix = mergeColumns(arrNewMatrix, arrMerge);
+    arrFixedMatrix = mergeColumnsFixed(arrFixedMatrix, arrMerge);
+  }
+  intYCounter = -1 * intProportionY / 2;
+  if (intNewHeight > intHeight) {
+    y = 0;
+    while (y < intNewHeight) {
+      intYCounter += intProportionY;
+      while (intYCounter > 1 && y < intNewHeight) {
+        arrNewMatrix = addLine(arrNewMatrix, y, 0);
+        arrFixedMatrix = addLine(arrFixedMatrix, y, 0);
+        intYCounter--;
+        y++;
+      }
+      y++;
+      intYCounter--;
+    }
+    while (arrNewMatrix[0].length < intNewHeight) {
+      intNewLine = arrNewMatrix[0].length - 1;
+      arrNewMatrix = addLine(arrNewMatrix, intNewLine, 0);
+      arrFixedMatrix = addLine(arrFixedMatrix, intNewLine, 100);
+      y++;
+    }
+  } else {
+    intYCounter = 0;
+    arrMerge = [];
+    y = 0;
+    yLoop = 0;
+    yGain = 0;
+    yWeight = 1;
+    intProportionY = intMatrixHeight / intNewHeight;
+    while (y < intNewHeight) {
+      intYCounter += intProportionY;
+      while (intYCounter > 0) {
+        arrMerge.push({
+          yBefore: yLoop,
+          yAfter: y,
+          weight: yWeight
+        });
+        intYCounter -= yWeight;
+        yGain += yWeight;
+        yWeight = 1 - yWeight;
+        if (yWeight === 0) {
+          yWeight = Math.min(1, intYCounter);
+        }
+        if (yGain === 1) {
+          yLoop++;
+          yWeight = Math.min(1, intYCounter);
+          yGain = 0;
+        }
+      }
+      y++;
+    }
+    arrNewMatrix = mergeLines(arrNewMatrix, arrMerge);
+    arrFixedMatrix = mergeLinesFixed(arrFixedMatrix, arrMerge);
+  }
+  resultMatrix = loopSmooth(arrNewMatrix, arrFixedMatrix);
+  debugMatrix(resultMatrix, arrFixedMatrix, strColor);
+  return resultMatrix;
+};
+
+createMatrix = function(intWidth, intHeight, dblValue) {
+  var arrNewMatrix, x, y;
+  arrNewMatrix = Array();
+  x = 0;
+  while (x < intWidth) {
+    arrNewMatrix[x] = Array();
+    y = 0;
+    while (y < intHeight) {
+      arrNewMatrix[x][y] = dblValue;
+      y++;
+    }
+    x++;
+  }
+  return arrNewMatrix;
+};
+
+createVector = function(intSize, dblValue) {
+  var arrVector, i;
+  arrVector = Array();
+  i = 0;
+  while (i < intSize) {
+    arrVector[i] = dblValue;
+    i++;
+  }
+  return arrVector;
+};
+
+addColumn = function(arrMatrix, intColumnPosition, dblValue) {
+  var arrNewMatrix, intLoopCount, x, xMatrix, y, yMatrix;
+  if (isNaN(dblValue)) {
+    throw new Error("invalid value");
+  }
+  arrNewMatrix = Array();
+  xMatrix = 0;
+  intLoopCount = 0;
+  x = 0;
+  while (x <= arrMatrix.length) {
+    intLoopCount++;
+    if (intLoopCount > 100) {
+      throw new Error("something bad happened");
+    }
+    if (x === intColumnPosition) {
+      arrNewMatrix[x] = createVector(arrMatrix[0].length, dblValue);
+      x++;
+    }
+    if (xMatrix < arrMatrix.length) {
+      arrNewMatrix[x] = Array();
+      yMatrix = 0;
+      y = 0;
+      while (y < arrMatrix[0].length) {
+        arrNewMatrix[x][y] = arrMatrix[xMatrix][yMatrix];
+        y++;
+        yMatrix++;
+      }
+    }
+    if (xMatrix > arrMatrix.length) {
+      throw new Error("to distance to create column. Length:" + arrMatrix.length + ", column:" + intColumnPosition);
+    }
+    x++;
+    xMatrix++;
+  }
+  return arrNewMatrix;
+};
+
+addLine = function(arrMatrix, intLinePosition, dblValue) {
+  var arrNewMatrix, intLoopCount, x, xMatrix, y, yMatrix;
+  arrNewMatrix = Array();
+  xMatrix = 0;
+  intLoopCount = 0;
+  x = 0;
+  while (x < arrMatrix.length) {
+    arrNewMatrix[x] = Array();
+    yMatrix = 0;
+    y = 0;
+    while (y <= arrMatrix[0].length) {
+      if (intLoopCount > 100) {
+        throw new Error("something bad happened");
+      }
+      if (y === intLinePosition) {
+        arrNewMatrix[x][y] = dblValue;
+        y++;
+      }
+      if (typeof arrMatrix[xMatrix][yMatrix] === "undefined" || arrMatrix[xMatrix][yMatrix] === null) {
+        if (yMatrix === arrMatrix[xMatrix].length) {
+          arrNewMatrix[x][y] = dblValue;
+        } else {
+          throw new Error("Unable to find the original value in the matrix");
+        }
+      } else {
+        arrNewMatrix[x][y] = arrMatrix[xMatrix][yMatrix];
+      }
+      y++;
+      yMatrix++;
+    }
+    x++;
+    xMatrix++;
+  }
+  return arrNewMatrix;
+};
+
+compareMatrix = function(arrMatrixA, arrMatrixB) {
+  var x, y;
+  if (arrMatrixA.length !== arrMatrixB.length) {
+    return false;
+  }
+  if (arrMatrixA[0].length !== arrMatrixB[0].length) {
+    return false;
+  }
+  x = 0;
+  while (x < arrMatrixA.length) {
+    y = 0;
+    while (y < arrMatrixA[0].length) {
+      if (arrMatrixA[x][y] !== arrMatrixB[x][y]) {
+        return false;
+      }
+      y++;
+    }
+    x++;
+  }
+  return true;
+};
+
+loopSmooth = function(arrMatrix, arrFixedMatrix) {
+  var arrNewMatrix, intCount;
+  arrNewMatrix = smoothMatrix(arrMatrix, arrFixedMatrix);
+  intCount = 0;
+  while (compareMatrix(arrNewMatrix, arrMatrix) === false) {
+    intCount++;
+    arrMatrix = arrNewMatrix;
+    arrNewMatrix = smoothMatrix(arrMatrix, arrFixedMatrix);
+    if (intCount > 500) {
+      return arrNewMatrix;
+    }
+  }
+  return arrNewMatrix;
+};
+
+smoothMatrix = function(arrMatrix, arrFixedMatrix) {
+  var arrElements, arrNewMatrix, arrVerticalElements, intHeight, intMaxX, intMaxY, intWidth, x, y;
+  arrNewMatrix = Array();
+  intWidth = arrMatrix.length;
+  intHeight = arrMatrix[0].length;
+  intMaxX = intWidth - 1;
+  intMaxY = intHeight - 1;
+  x = 0;
+  while (x < arrMatrix.length) {
+    arrNewMatrix[x] = Array();
+    y = 0;
+    while (y < arrMatrix[0].length) {
+      if (arrFixedMatrix[x][y] !== 1) {
+
+        /**
+        -+ A    0+ B    ++ C
+        -0 D    00 E    +0 F
+        -- G    0- H    +- I
+         */
+        arrElements = Array();
+        arrVerticalElements = Array();
+        if (x > 0 && y < intMaxY) {
+          arrVerticalElements.push(arrMatrix[x - 1][y + 1]);
+        }
+        if (y < intMaxY) {
+          arrElements.push(arrMatrix[x][y + 1]);
+        }
+        if (x < intMaxX && y < intMaxY) {
+          arrVerticalElements.push(arrMatrix[x + 1][y + 1]);
+        }
+        if (x > 0) {
+          arrElements.push(arrMatrix[x - 1][y]);
+        }
+        arrElements.push(arrMatrix[x][y]);
+        if (x < intMaxX) {
+          arrElements.push(arrMatrix[x + 1][y]);
+        }
+        if (x > 0 && y > 0) {
+          arrVerticalElements.push(arrMatrix[x - 1][y - 1]);
+        }
+        if (y > 0) {
+          arrElements.push(arrMatrix[x][y - 1]);
+        }
+        if (x < intMaxX && y > 0) {
+          arrVerticalElements.push(arrMatrix[x + 1][y - 1]);
+        }
+        arrNewMatrix[x][y] = Math.round(((1000 * arrElements.sum()) + (1000 * arrVerticalElements.sum() / 1.4142)) / (arrElements.length + (arrVerticalElements.length / 1.4142))) / 1000;
+        if (isNaN(arrNewMatrix[x][y])) {
+          throw new Error("That's why i hate javascript");
+        }
+      } else {
+        arrNewMatrix[x][y] = arrMatrix[x][y];
+      }
+      y++;
+    }
+    x++;
+  }
+  return arrNewMatrix;
+};
+
+mergeColumns = function(arrMatrix, arrMerge) {
+  var arrNewMatrix, i, newValue, objMerge, totalWeight, x, y;
+  arrNewMatrix = [];
+  totalWeight = 0;
+  i = 0;
+  while (i < arrMerge.length) {
+    objMerge = arrMerge[i];
+    y = 0;
+    while (y < arrMatrix[0].length) {
+      if (arrNewMatrix[objMerge.xAfter] === undefined) {
+        arrNewMatrix[objMerge.xAfter] = [];
+      }
+      if (arrNewMatrix[objMerge.xAfter][y] === undefined) {
+        arrNewMatrix[objMerge.xAfter][y] = [];
+      }
+      if (y === 0 && objMerge.xAfter === 0) {
+        totalWeight += objMerge.weight;
+      }
+      if (arrMatrix[objMerge.xBefore] !== undefined) {
+        newValue = arrMatrix[objMerge.xBefore][y] * objMerge.weight;
+        arrNewMatrix[objMerge.xAfter][y].push(newValue);
+      }
+      y++;
+    }
+    i++;
+  }
+  x = 0;
+  while (x < arrNewMatrix.length) {
+    y = 0;
+    while (y < arrNewMatrix[0].length) {
+      arrNewMatrix[x][y] = arrNewMatrix[x][y].sum() / totalWeight;
+      y++;
+    }
+    x++;
+  }
+  return arrNewMatrix;
+};
+
+mergeColumnsFixed = function(arrMatrix, arrMerge) {
+  var arrNewMatrix = mergeColumns( arrMatrix, arrMerge );
+  for( var x = 0; x < arrNewMatrix.length; x++ ) {
+    for( var y = 0; y < arrNewMatrix[0].length; y++ ) {
+      arrNewMatrix[x][y] = Math.round( arrNewMatrix[x][y] ) == 1 ? 1 : 0
+    }
+  }
+  return arrNewMatrix;
+}
+
+mergeLines = function(arrMatrix, arrMerge) {
+  var arrNewMatrix, i, newValue, objMerge, totalWeight, x, y;
+  arrNewMatrix = [];
+  totalWeight = 0;
+  i = 0;
+  while (i < arrMerge.length) {
+    objMerge = arrMerge[i];
+    x = 0;
+    while (x < arrMatrix.length) {
+      if (arrNewMatrix[x] === undefined) {
+        arrNewMatrix[x] = [];
+      }
+      if (arrNewMatrix[x][objMerge.yAfter] === undefined) {
+        arrNewMatrix[x][objMerge.yAfter] = [];
+      }
+      if (x === 0 && objMerge.yAfter === 0) {
+        totalWeight += objMerge.weight;
+      }
+      if (arrMatrix[x][objMerge.yBefore] !== undefined) {
+        newValue = arrMatrix[x][objMerge.yBefore] * objMerge.weight;
+        arrNewMatrix[x][objMerge.yAfter].push(newValue);
+      }
+      x++;
+    }
+    i++;
+  }
+  x = 0;
+  while (x < arrNewMatrix.length) {
+    y = 0;
+    while (y < arrNewMatrix[0].length) {
+      arrNewMatrix[x][y] = arrNewMatrix[x][y].sum() / totalWeight;
+      y++;
+    }
+    x++;
+  }
+  return arrNewMatrix;
+};
+
+mergeLinesFixed = function(arrMatrix, arrMerge) {
+  var arrNewMatrix = mergeLines( arrMatrix, arrMerge );
+  for( var x = 0; x < arrNewMatrix.length; x++ ) {
+    for( var y = 0; y < arrNewMatrix[0].length; y++ ) {
+      arrNewMatrix[x][y] = Math.round( arrNewMatrix[x][y] ) == 1 ? 1 : 0
+    }
+  }
+  return arrNewMatrix;
+}
+
+getSumCircle = function( arrMatrix, objCenter, radiusX, radiusY, newValue ) {
+  var intYMin = objCenter.y - Math.floor( radiusY );
+  var intYMax = objCenter.y + Math.floor( radiusY );
+  var intXMax = objCenter.x + Math.floor( radiusX );
+  var intXMin = objCenter.x - Math.floor( radiusX );
+  var sum = 0;
+  console.log( "center = ", objCenter );
+//  arrMatrix[objCenter.x][objCenter.y] = newValue;
+
+  for( var x = intXMin; x < intXMax; x++ ){
+    for( var y = intYMin; y < intYMax; y++ ){
+      console.log( "add["+x+","+y+"] = " );
+      if( arrMatrix[x] !== undefined && arrMatrix[x][y] !== undefined ) {
+        console.log( arrMatrix[x][y] );
+        sum += arrMatrix[x][y];
+//        arrMatrix[x][y] = newValue;
+      } else {
+        console.log("isto not exists!");
+      }
+    }
+  }
+  return sum
+}
+
+getHeavy = function(arrMatrix) {
+  var intWidth = arrMatrix.length;
+  var intHeight = arrMatrix[0].length;
+  var fltWidthRadius  = Math.round( intWidth  / 4 );
+  var fltHeightRadius = Math.round( intHeight / 4 );
+  var objCenter = { 
+    x: Math.floor(  intWidth / 2 ), 
+    y: Math.floor( intHeight / 2 ) 
+  };
+  var intSteps = 36;
+  var arrHeavy = [];
+  for( var i = 0; i < intSteps; i++ ) {
+    var currentAngle = 2 * Math.PI * ( i / intSteps );
+    console.log( "angle = ", currentAngle );
+//    console.log( "objCenter = ", objCenter );
+    var addXCos, addXSin;
+    addXCos = fltWidthRadius  * Math.cos( currentAngle );
+    addXSin = 0;//fltHeightRadius * Math.sin( currentAngle );
+
+    var addYCos, addYSin;
+    addYCos = fltHeightRadius  * Math.sin( currentAngle );
+    addYSin = 0;// fltWidthRadius   * Math.sin( currentAngle );
+//    console.log( "cos = ", Math.cos( currentAngle ) );
+//    console.log( "sin = ", Math.sin( currentAngle ) );
+//    console.log( "addXCos = ",addXCos );
+//    console.log( "addXSin = ",addXSin );
+    var x = Math.round( objCenter.x + addXCos + addXSin );
+    var y = Math.round( objCenter.y + addYCos + addYSin );
+    var sum = getSumCircle( arrMatrix, {x: x, y: y}, fltWidthRadius , fltHeightRadius, 255 * ( i / intSteps ) );
+    arrHeavy.push( sum );
+  }
+  return arrHeavy;
+}
+
+Array.prototype.sum = function() {
+  var L, i, sum;
+  i = 0;
+  L = this.length;
+  sum = 0;
+  while (i < L) {
+    sum += this[i++];
+  }
+  return sum;
+};
